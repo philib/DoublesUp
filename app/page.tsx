b62649bac7ded63cd739975349331bf7547d2c89
philib <00philip00@gmail.com>
2023-06-18 17:34:11 +0200

    add simple meldeliste

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,28 @@
 'use client'
 import {Button, Input} from "@mui/joy";
+import {useState} from "react";
 
 export default function Home() {
 
-
+    const [inputText, setInputText] = useState("")
+    const [meldeliste, setMeldeListe] = useState<string[]>([])
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <form
                 onSubmit={(event) => {
+                    setMeldeListe([...meldeliste, inputText])
                     event.preventDefault();
                 }}
             >
                 <Input
                     placeholder="Try to submit with no text!"
                     required
+                    onChange={(t) => setInputText(t.target.value)}
                     sx={{mb: 1, fontSize: 'var(--joy-fontSize-sm)'}}
                 />
                 <Button type="submit">Submit</Button>
             </form>
+            {meldeliste.map((entry) => <div key={entry}>{entry}</div>)}
         </main>
     )
 }
