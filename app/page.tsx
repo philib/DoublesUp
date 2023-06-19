698d23e243ddcb239ae4317ace849fe885143bb3
philib <00philip00@gmail.com>
2023-06-19 23:00:00 +0200

    rename variables

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
 
     const pairings = [[[1, 2], [3, 4], [5, 6]], [[1, 2], [3, 5], [4, 6]], [[1, 2], [3, 6], [4, 5]], [[1, 2], [4, 5], [3, 6]], [[1, 3], [2, 4], [5, 6]], [[1, 3], [2, 5], [4, 6]], [[1, 3], [2, 6], [4, 5]], [[1, 4], [2, 3], [5, 6]], [[1, 4], [2, 5], [3, 6]], [[1, 4], [2, 6], [3, 5]], [[1, 4], [3, 5], [2, 6]], [[1, 5], [2, 4], [3, 6]], [[1, 5], [3, 4], [2, 6]], [[1, 6], [2, 5], [3, 4]], [[1, 6], [3, 4], [2, 5]], [[2, 3], [1, 4], [5, 6]], [[2, 3], [1, 5], [4, 6]], [[2, 3], [1, 6], [4, 5]], [[2, 4], [1, 5], [3, 6]], [[2, 4], [1, 6], [3, 5]], [[2, 5], [1, 6], [3, 4]], [[3, 4], [1, 6], [2, 5]]]
     const [inputText, setInputText] = useState("")
-    const [meldeliste, setMeldeListe] = useState<string[]>(["A", "B", "C", "D", "E", "F"])
-    const [aufstellung, setAufstellung] = useState<Record<number, string>>({})
-    const ready = Object.values(aufstellung).length == 6
+    const [players, setPlayers] = useState<string[]>(["A", "B", "C", "D", "E", "F"])
+    const [lineup, setLineup] = useState<Record<number, string>>({})
+    const ready = Object.values(lineup).length == 6
     const [filters, setFilters] = useState<string[]>([])
     const uniquePairs = _.sortBy(_.uniqBy(pairings.flat(), pair => `${pair[0]}, ${pair[1]}`), p => p[0]).map((doppel, doppelIndex) => {
 
@@ -30,7 +30,7 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <form
                 onSubmit={(event) => {
-                    setMeldeListe([...meldeliste, inputText])
+                    setPlayers([...players, inputText])
                     event.preventDefault();
                 }}
             >
@@ -43,16 +43,16 @@ export default function Home() {
                 <Button type="submit">Submit</Button>
             </form>
             <div>
-                {meldeliste.map((entry, index) => <p key={entry}><Checkbox onChange={(e) => {
+                {players.map((entry, index) => <p key={entry}><Checkbox onChange={(e) => {
                     if (e.target.checked) {
-                        setAufstellung({...aufstellung, [index + 1]: entry})
+                        setLineup({...lineup, [index + 1]: entry})
                     } else {
-                        setAufstellung(_.omit(aufstellung, [index + 1]))
+                        setLineup(_.omit(lineup, [index + 1]))
                     }
                 }}/> {entry} <Button variant="contained"
                                      onClick={() => {
-                                         setMeldeListe(_.without(meldeliste, entry))
-                                         setAufstellung(_.omit(aufstellung, [index + 1]))
+                                         setPlayers(_.without(players, entry))
+                                         setLineup(_.omit(lineup, [index + 1]))
                                      }}> Remove </Button></p>)}
             </div>
             <div>
@@ -67,7 +67,7 @@ export default function Home() {
                         <List>
                             {pairings.map((doppelaufstellung, index) => <List key={index}> Variante {index}
                                 {doppelaufstellung.map(doppel =>
-                                    <ListItem>{Object.values(aufstellung)[doppel[0] - 1]} + {Object.values(aufstellung)[doppel[1] - 1]}</ListItem>)}</List>)}
+                                    <ListItem>{Object.values(lineup)[doppel[0] - 1]} + {Object.values(lineup)[doppel[1] - 1]}</ListItem>)}</List>)}
                             <Divider/>
                         </List>
                     </>
