880f1e575362f50f68e4e2b5ac4d5407b4336b22
philib <00philip00@gmail.com>
2023-06-19 23:54:45 +0200

    rename variables

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import _ from "lodash";
 import {Button, Checkbox, List, ListItem, Divider, Chip} from "@mui/material";
 
 export default function Home() {
-    const pairings = [[[1, 2], [3, 4], [5, 6]], [[1, 2], [3, 5], [4, 6]], [[1, 2], [3, 6], [4, 5]], [[1, 2], [4, 5], [3, 6]], [[1, 3], [2, 4], [5, 6]], [[1, 3], [2, 5], [4, 6]], [[1, 3], [2, 6], [4, 5]], [[1, 4], [2, 3], [5, 6]], [[1, 4], [2, 5], [3, 6]], [[1, 4], [2, 6], [3, 5]], [[1, 4], [3, 5], [2, 6]], [[1, 5], [2, 4], [3, 6]], [[1, 5], [3, 4], [2, 6]], [[1, 6], [2, 5], [3, 4]], [[1, 6], [3, 4], [2, 5]], [[2, 3], [1, 4], [5, 6]], [[2, 3], [1, 5], [4, 6]], [[2, 3], [1, 6], [4, 5]], [[2, 4], [1, 5], [3, 6]], [[2, 4], [1, 6], [3, 5]], [[2, 5], [1, 6], [3, 4]], [[3, 4], [1, 6], [2, 5]]]
+    const allPossibleLineupVariations = [[[1, 2], [3, 4], [5, 6]], [[1, 2], [3, 5], [4, 6]], [[1, 2], [3, 6], [4, 5]], [[1, 2], [4, 5], [3, 6]], [[1, 3], [2, 4], [5, 6]], [[1, 3], [2, 5], [4, 6]], [[1, 3], [2, 6], [4, 5]], [[1, 4], [2, 3], [5, 6]], [[1, 4], [2, 5], [3, 6]], [[1, 4], [2, 6], [3, 5]], [[1, 4], [3, 5], [2, 6]], [[1, 5], [2, 4], [3, 6]], [[1, 5], [3, 4], [2, 6]], [[1, 6], [2, 5], [3, 4]], [[1, 6], [3, 4], [2, 5]], [[2, 3], [1, 4], [5, 6]], [[2, 3], [1, 5], [4, 6]], [[2, 3], [1, 6], [4, 5]], [[2, 4], [1, 5], [3, 6]], [[2, 4], [1, 6], [3, 5]], [[2, 5], [1, 6], [3, 4]], [[3, 4], [1, 6], [2, 5]]]
     const [inputText, setInputText] = useState("")
     const [players, setPlayers] = useState<string[]>(["A", "B", "C", "D", "E", "F"])
     const [lineup, setLineup] = useState<Record<number, string>>({})
     const ready = Object.values(lineup).length == 6
-    const [filters, setFilters] = useState<number[][]>([])
+    const [selectedDoublesPairingFilter, setSelectedDoublesPairingFilter] = useState<number[][]>([])
 
     const isEqual = (pair1: number[]) => {
         return (pair2 : number[] ) => pair1[0] == pair2[0] && pair1[1] == pair2[1];
@@ -19,12 +19,12 @@ export default function Home() {
         return (pair2 : number[] ) => pair1[0] != pair2[0] || pair1[1] != pair2[1];
     }
 
-    const allFilterCombinedPredicate = filters
-        .map(doppel => (lineup: number[][]) => lineup.find(isEqual(doppel)) != undefined)
+    const allSelectedDoublesPairingFiltersCombinedPredicate = selectedDoublesPairingFilter
+        .map(doublesPairing => (lineup: number[][]) => lineup.find(isEqual(doublesPairing)) != undefined)
         .reduce((acc, curr) => (lineup: number[][]) => acc(lineup) && curr(lineup), (lineup: number[][]) => true)
 
-    let filteredPairings = pairings.filter(allFilterCombinedPredicate);
-    const remainingFilters = _.uniqBy(filteredPairings.flat(), pair => `${pair[0]}+${pair[1]}`)
+    let filteredLineupVariations = allPossibleLineupVariations.filter(allSelectedDoublesPairingFiltersCombinedPredicate);
+    const remainingDoublesPairingFilters = _.uniqBy(filteredLineupVariations.flat(), pair => `${pair[0]}+${pair[1]}`)
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -57,16 +57,16 @@ export default function Home() {
             </div>
             <div>
                 {
-                    ready && remainingFilters.map((doppel, doppelIndex) => {
-                        const labelText = `${Object.values(lineup)[doppel[0] - 1]} & ${Object.values(lineup)[doppel[1] - 1]}`
-                        const active = filters.find(isEqual(doppel)) != undefined
+                    ready && remainingDoublesPairingFilters.map((doublePairingFilter, index) => {
+                        const labelText = `${Object.values(lineup)[doublePairingFilter[0] - 1]} & ${Object.values(lineup)[doublePairingFilter[1] - 1]}`
+                        const active = selectedDoublesPairingFilter.find(isEqual(doublePairingFilter)) != undefined
                         if (active) {
-                            return <Chip key={doppelIndex} label={labelText} onDelete={() => {
-                                setFilters(filters.filter(isNotEqual(doppel)))
+                            return <Chip key={index} label={labelText} onDelete={() => {
+                                setSelectedDoublesPairingFilter(selectedDoublesPairingFilter.filter(isNotEqual(doublePairingFilter)))
                             }}/>
                         } else {
-                            return <Chip key={doppelIndex} label={labelText} onClick={() => {
-                                setFilters([...filters, doppel])
+                            return <Chip key={index} label={labelText} onClick={() => {
+                                setSelectedDoublesPairingFilter([...selectedDoublesPairingFilter, doublePairingFilter])
                             }}/>
                         }
                     })
@@ -77,9 +77,9 @@ export default function Home() {
                     ready && <>
                         Aufstellung:
                         <List>
-                            {filteredPairings.map((doppelaufstellung, index) => <List key={index}> Variante {index}
-                                {doppelaufstellung.map(doppel =>
-                                    <ListItem>{Object.values(lineup)[doppel[0] - 1]} + {Object.values(lineup)[doppel[1] - 1]}</ListItem>)}</List>)}
+                            {filteredLineupVariations.map((lineupVariation, index) => <List key={index}> Variante {index}
+                                {lineupVariation.map(doublesPairing =>
+                                    <ListItem>{Object.values(lineup)[doublesPairing[0] - 1]} + {Object.values(lineup)[doublesPairing[1] - 1]}</ListItem>)}</List>)}
                             <Divider/>
                         </List>
                     </>
