f6d259a3b44f203c3b54484ea5fca350b75f691f
philib <00philip00@gmail.com>
2023-06-19 23:46:01 +0200

    combine all filters once within a single predicate

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,25 @@ import _ from "lodash";
 import {Button, Checkbox, List, ListItem, Divider, Chip} from "@mui/material";
 
 export default function Home() {
-
     const pairings = [[[1, 2], [3, 4], [5, 6]], [[1, 2], [3, 5], [4, 6]], [[1, 2], [3, 6], [4, 5]], [[1, 2], [4, 5], [3, 6]], [[1, 3], [2, 4], [5, 6]], [[1, 3], [2, 5], [4, 6]], [[1, 3], [2, 6], [4, 5]], [[1, 4], [2, 3], [5, 6]], [[1, 4], [2, 5], [3, 6]], [[1, 4], [2, 6], [3, 5]], [[1, 4], [3, 5], [2, 6]], [[1, 5], [2, 4], [3, 6]], [[1, 5], [3, 4], [2, 6]], [[1, 6], [2, 5], [3, 4]], [[1, 6], [3, 4], [2, 5]], [[2, 3], [1, 4], [5, 6]], [[2, 3], [1, 5], [4, 6]], [[2, 3], [1, 6], [4, 5]], [[2, 4], [1, 5], [3, 6]], [[2, 4], [1, 6], [3, 5]], [[2, 5], [1, 6], [3, 4]], [[3, 4], [1, 6], [2, 5]]]
     const [inputText, setInputText] = useState("")
     const [players, setPlayers] = useState<string[]>(["A", "B", "C", "D", "E", "F"])
     const [lineup, setLineup] = useState<Record<number, string>>({})
     const ready = Object.values(lineup).length == 6
     const [filters, setFilters] = useState<number[][]>([])
-    console.log(filters)
-    const uniquePairs =
-        _.sortBy(
-            _.uniqBy(pairings.flat(), pair => `${pair[0]}, ${pair[1]}`), p => p[0]
-        )
-    const filterPairing = (pairing: number[][]) => filters.reduce((acc, curr) => {
-        return acc && pairing.find(pair => pair[0] == curr[0] && pair[1] == curr[1]) != undefined
-    }, true)
 
-    let filteredPairings = pairings.filter(filterPairing);
+    const isEqual = (pair1: number[]) => {
+        return (pair2 : number[] ) => pair1[0] == pair2[0] && pair1[1] == pair2[1];
+    }
+    const isNotEqual = (pair1: number[]) => {
+        return (pair2 : number[] ) => pair1[0] != pair2[0] || pair1[1] != pair2[1];
+    }
+
+    const allFilterCombinedPredicate = filters
+        .map(doppel => (lineup: number[][]) => lineup.find(isEqual(doppel)) != undefined)
+        .reduce((acc, curr) => (lineup: number[][]) => acc(lineup) && curr(lineup), (lineup: number[][]) => true)
+
+    let filteredPairings = pairings.filter(allFilterCombinedPredicate);
     const remainingFilters = _.uniqBy(filteredPairings.flat(), pair => `${pair[0]}+${pair[1]}`)
 
     return (
@@ -56,11 +58,11 @@ export default function Home() {
             <div>
                 {
                     ready && remainingFilters.map((doppel, doppelIndex) => {
-                        const labelText = `${Object.values(lineup)[doppel[0]-1]} & ${Object.values(lineup)[doppel[1]-1]}`
-                        const active = filters.find(f => f[0] == doppel[0] && f[1] == doppel[1]) != undefined
+                        const labelText = `${Object.values(lineup)[doppel[0] - 1]} & ${Object.values(lineup)[doppel[1] - 1]}`
+                        const active = filters.find(isEqual(doppel)) != undefined
                         if (active) {
                             return <Chip key={doppelIndex} label={labelText} onDelete={() => {
-                                setFilters(filters.filter(f => f[0] != doppel[0] || f[1] != doppel[1]))
+                                setFilters(filters.filter(isNotEqual(doppel)))
                             }}/>
                         } else {
                             return <Chip key={doppelIndex} label={labelText} onClick={() => {
