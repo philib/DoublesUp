27a65fb57739228b79be01216247057fca03ce55
philib <00philip00@gmail.com>
2024-01-28 15:13:55 +0100

    add RegistriationList domain model

---
+++ app/RegistrationList.ts
@@ -0,0 +1,73 @@
+export class PlayerId {
+  value: string;
+  public static create(id: string): PlayerId {
+    return new PlayerId(id);
+  }
+  private constructor(value: string) {
+    this.value = value;
+  }
+}
+
+export interface Player {
+  id: PlayerId;
+  name: string;
+}
+
+export class RegistrationList {
+  private registrationList: { [rank: number]: Player } = {};
+  public static create(input: {
+    [rank: number]: Player;
+  }): RegistrationList | RegistrationListCreateError {
+    const uniqueIds = new Set(
+      Object.values(input).map((player) => player.id.value)
+    ).size;
+    if (uniqueIds !== Object.values(input).length) {
+      return 'PlayerNotUnique';
+    }
+    return new RegistrationList(input);
+  }
+  private constructor(list: { [rank: number]: Player }) {
+    this.registrationList = list;
+  }
+
+  addPlayer(
+    rank: number,
+    player: Player
+  ): RegistrationList | RegistrationListAddPlayerError {
+    if (this.registrationList[rank]?.id.value === player.id.value) {
+      return this;
+    }
+    if (this.registrationList[rank]) {
+      return 'Rank already taken';
+    }
+    if (this.playerExists(player)) {
+      return 'Player already exists';
+    }
+
+    return new RegistrationList({ ...this.registrationList, [rank]: player });
+  }
+  private playerExists(player: Player): boolean {
+    return (
+      Object.values(this.registrationList).find((p) => p.id === player.id) !==
+      undefined
+    );
+  }
+
+  getList(): { [rank: number]: Player } {
+    return this.registrationList;
+  }
+
+  removePlayer(id: PlayerId): RegistrationList {
+    const filtered = Object.entries(this.registrationList).filter(
+      ([key, value]) => {
+        return value.id.value !== id.value;
+      }
+    );
+    return new RegistrationList(Object.fromEntries(filtered));
+  }
+}
+
+type RegistrationListCreateError = 'PlayerNotUnique';
+export type RegistrationListAddPlayerError =
+  | 'Player already exists'
+  | 'Rank already taken';
---
+++ test/RegistrationList.test.ts
@@ -0,0 +1,92 @@
+import { Player, PlayerId, RegistrationList } from '../app/RegistrationList';
+function createValidRegistrationList(list: {
+  [rank: number]: Player;
+}): RegistrationList {
+  const registrationList = RegistrationList.create(list);
+  expect(registrationList).toBeInstanceOf(RegistrationList);
+  return registrationList as RegistrationList;
+}
+function createPlayer(name: string): Player {
+  return {
+    id: PlayerId.create(name),
+    name,
+  };
+}
+describe('create a registration list', () => {
+  test('non unique id return error', () => {
+    const id = PlayerId.create('1');
+    const list = RegistrationList.create({
+      1: { id, name: 'Test' },
+      2: { id, name: 'Test2' },
+    });
+    expect(list).toBe('PlayerNotUnique');
+  });
+
+  test('unique id return list', () => {
+    const id1 = PlayerId.create('1');
+    const id2 = PlayerId.create('2');
+    const list = RegistrationList.create({
+      1: { id: id1, name: 'Test' },
+      2: { id: id2, name: 'Test2' },
+    });
+    expect(list).toBeInstanceOf(RegistrationList);
+    expect((list as RegistrationList).getList()).toEqual({
+      1: { id: id1, name: 'Test' },
+      2: { id: id2, name: 'Test2' },
+    });
+  });
+});
+
+describe('remove player', () => {
+  test('remove not existing player returns registration list', () => {
+    const registrationList = createValidRegistrationList({
+      1: createPlayer('Test'),
+    });
+    const result = registrationList.removePlayer(PlayerId.create('someId'));
+    expect(result.getList()).toEqual(registrationList.getList());
+  });
+
+  test('removes existing player', () => {
+    const existingPlayer = createPlayer('Test');
+    const registrationList = createValidRegistrationList({
+      1: existingPlayer,
+    });
+    const result = registrationList.removePlayer(PlayerId.create('someId'));
+    expect(result.getList()).toEqual(registrationList.getList());
+  });
+
+  describe('add player', () => {
+    test('adds player', () => {
+      const registrationList = createValidRegistrationList({});
+      const player1 = createPlayer('Player 1');
+      const result = registrationList.addPlayer(1, player1) as RegistrationList;
+      expect(result).toBeInstanceOf(RegistrationList);
+      expect(result.getList()).toEqual({ 1: player1 });
+    });
+
+    test('cannot add player with same id twice', () => {
+      const player1 = createPlayer('Player 1');
+      const registrationList = createValidRegistrationList({ 1: player1 });
+      const result = registrationList.addPlayer(2, player1);
+      expect(result).toBe('Player already exists');
+    });
+
+    test('cannot add player on already ranked position', () => {
+      const player1 = createPlayer('Player 1');
+      const registrationList = createValidRegistrationList({ 1: player1 });
+      const player2 = createPlayer('Player 2');
+      const result = registrationList.addPlayer(1, player2);
+      expect(result).toBe('Rank already taken');
+    });
+
+    test('can rank player on current rank', () => {
+      const player1 = createPlayer('Player 1');
+      const registrationList = createValidRegistrationList({ 1: player1 });
+      const result = registrationList.addPlayer(1, player1);
+      expect(result).toBeInstanceOf(RegistrationList);
+      expect((result as RegistrationList).getList()).toEqual({
+        1: player1,
+      });
+    });
+  });
+});
