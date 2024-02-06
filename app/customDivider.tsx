ec0ea244d088a5794cabbe647cb41605685a49f3
philib <00philip00@gmail.com>
2024-02-06 18:29:51 +0100

    reformat

--- app/customDivider.tsx
+++ app/customDivider.tsx
@@ -1,5 +1,8 @@
-import React, {ReactNode} from "react";
-import {Divider} from "@mui/material";
+import React, { ReactNode } from 'react';
+import { Divider } from '@mui/material';
 
 export const CustomDivider: React.FC<{ children?: ReactNode }> = (props) => (
-    <Divider style={{marginTop: '20px', marginBottom: '20px'}}>{props.children}</Divider>)
+  <Divider style={{ marginTop: '20px', marginBottom: '20px' }}>
+    {props.children}
+  </Divider>
+);
