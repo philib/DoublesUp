a153f65805b868fa9fc74bb7195b3cb016a54509
philib <00philip00@gmail.com>
2024-02-29 18:21:18 +0100

    move fake service hook to stories

--- stories/useFakeService.tsx
+++ stories/useFakeService.tsx
@@ -1,5 +1,9 @@
-import { PlayerId } from '../app/RegistrationList';
-import { createUseService } from '../app/service/useRegistrationListServiceFactory';
+import { useState } from 'react';
+import { v4 } from 'uuid';
+import { Player, PlayerId } from '../app/RegistrationList';
+import { MeldelistePlayer } from '../app/components/Meldeliste/Meldeliste';
+import { RegistrationListRepository } from '../app/repository/RegistrationListRepository';
+import { RegistrationListService } from '../app/service/RegistrationListService';
 import { RegistrationListRepositoryFake } from '../test/service/RegistrationListRepositoryFake';
 
 export const fakeRepository = (playersCount: number) =>
@@ -17,3 +21,59 @@ export const fakeRepository = (playersCount: number) =>
   );
 export const createFakeUseService = (playersCount: number) =>
   createUseService(fakeRepository(playersCount));
+
+const createUseService = (repo: RegistrationListRepository) => () => {
+  const service = new RegistrationListService(repo);
+  const toMeldeliste = (service: RegistrationListService) =>
+    Object.entries(service.getList().getList()).map(([key, value]) => ({
+      id: value.id,
+      name: value.name,
+      rank: Number(key),
+    }));
+  const [players, setPlayers] = useState<MeldelistePlayer[]>(
+    toMeldeliste(service)
+  );
+  return {
+    players,
+    addPlayer: (rank: number, name: string) => {
+      service.addPlayer(rank, { name, id: PlayerId.create(v4()) });
+      setPlayers(toMeldeliste(service));
+    },
+    editPlayer: (player: MeldelistePlayer) => {
+      if (
+        service.editPlayer(player.id, {
+          name: player.name,
+          rank: player.rank,
+        }) !== 'Rank already taken'
+      ) {
+        setPlayers(toMeldeliste(service));
+        return 'SUCCESS';
+      } else {
+        return 'Rank already taken';
+      }
+    },
+    deletePlayer: (id: PlayerId) => {
+      service.removePlayer(id);
+      setPlayers(toMeldeliste(service));
+    },
+    sortPlayer: (rankFrom: number, rankTo: number) => {
+      service.sortPlayer(rankFrom, rankTo);
+      setPlayers(toMeldeliste(service));
+    },
+    getPlayerById: (id: PlayerId): Player | undefined => {
+      return service.getPlayerById(id);
+    },
+    selectPlayer(id: PlayerId) {
+      service.selectPlayer(id);
+    },
+    deselectPlayer(id: PlayerId) {
+      service.deselectPlayer(id);
+    },
+    getPlayerSelection(): Player[] {
+      return service.getPlayerSelection();
+    },
+  };
+};
+function uuidv4(): string {
+  throw new Error('Function not implemented.');
+}
