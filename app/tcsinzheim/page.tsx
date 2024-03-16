39e7c38a4874df1c6dda9e6d6c48fe4dcb950ac4
philib <00philip00@gmail.com>
2024-03-16 11:07:36 +0100

    fix image lint errors

--- app/tcsinzheim/page.tsx
+++ app/tcsinzheim/page.tsx
@@ -2,18 +2,23 @@
 import React from 'react';
 
 import teams from './teams.json';
-import { MyIntlProvider } from '../MyIntlProvider';
-import { StaticApp } from '../components/App/StaticApp';
-import { readTeams } from '../teams/readTeams';
+import {MyIntlProvider} from '../MyIntlProvider';
+import {StaticApp} from '../components/App/StaticApp';
+import {readTeams} from '../teams/readTeams';
 import Logo from './logo.png';
+import Image from 'next/image'
 
 const Home: React.FC = (props) => {
-  return (
-    <main>
-      <MyIntlProvider>
-        <StaticApp icon={<img src={Logo.src} height={'65px'} />} teams={readTeams(teams)} {...props} />
-      </MyIntlProvider>
-    </main>
-  );
+    const scale = 0.038;
+
+    return (
+        <main>
+            <MyIntlProvider>
+                <StaticApp
+                    icon={<Image src={Logo.src} height={Logo.height * scale} width={Logo.width * scale} alt={"logo"}/>}
+                    teams={readTeams(teams)} {...props} />
+            </MyIntlProvider>
+        </main>
+    );
 };
 export default Home;
